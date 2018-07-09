a33c892843fc98d7a5a484d72c847c111269b99b
jojoldu <jojoldu@gmail.com>
2018-07-09 22:21:26 +0900

    14장 디버깅 코드 완성

--- src/main/java/com/jojoldu/inflearn/intellij/community/chap2/special/FocusError.java
+++ src/main/java/com/jojoldu/inflearn/intellij/community/chap2/special/FocusError.java
@@ -2,10 +2,4 @@ package com.jojoldu.inflearn.intellij.community.chap2.special;
 
 public class FocusError {
 
-    public void moveError() {
-        return "";
-    }
-    public void moveError() {
-        return "";
-    }
 }
--- src/main/java/com/jojoldu/inflearn/intellij/community/chap5/Board.java
+++
@@ -1,8 +0,0 @@
-package com.jojoldu.inflearn.intellij.community.chap5;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-public class Board {
-
-}

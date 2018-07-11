6a8ab56f8ff92ef7fc064dfa69511a78284a453e
jojoldu <jojoldu@gmail.com>
2018-07-11 22:07:54 +0900

    커밋 포맷 롤백

--- src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Reformat.java
+++ src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Reformat.java
@@ -2,7 +2,5 @@ package com.jojoldu.inflearn.intellij.community.chap8;
 
 public class Reformat {
 
-    public void reformat() {
-        System.out.println("aa");
-    }
+    public void reformat() { System.out.println("aa"); }
 }

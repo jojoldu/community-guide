0849c9ebbc170f74da191da02e6987131150ec6b
jojoldu <jojoldu@gmail.com>
2018-07-12 22:20:17 +0900

    Before commit 확인

--- src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Diff.java
+++ src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Diff.java
@@ -3,6 +3,7 @@ package com.jojoldu.inflearn.intellij.community.chap8;
 public class Diff {
     public void diff() {
         System.out.println("diff-1");
+        System.out.println("diff-1");
         System.out.println("diff-1-1");
         System.out.println("diff-2");
         System.out.println("diff-3");

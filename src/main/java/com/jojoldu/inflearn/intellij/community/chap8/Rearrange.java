20a31b68e1635674ca174e18245046ffe50654b3
jojoldu <jojoldu@gmail.com>
2018-07-12 22:21:45 +0900

    정상 상태로 변경

--- src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Rearrange.java
+++ src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Rearrange.java
@@ -2,11 +2,11 @@ package com.jojoldu.inflearn.intellij.community.chap8;
 
 public class Rearrange {
 
-    public void addAge(int age) {
-        this.age += age;
-    }
-
     private String name;
 
     private int age;
+
+    public void addAge(int age) {
+        this.age += age;
+    }
 }
--- src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Reformat.java
+++ src/main/java/com/jojoldu/inflearn/intellij/community/chap8/Reformat.java
@@ -2,5 +2,7 @@ package com.jojoldu.inflearn.intellij.community.chap8;
 
 public class Reformat {
 
-    public void reformat() { System.out.println("aa"); }
+    public void reformat() {
+        System.out.println("aa");
+    }
 }
